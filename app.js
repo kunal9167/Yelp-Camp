4cf6a70645af9492d41e07555f3b1f896f436d1f
Kunal Gupta <kunal9167@gmail.com>
2018-05-23 01:14:01 +0530

    Added Authentication

--- app.js
+++ app.js
@@ -6,44 +6,43 @@ var Campground = require("./models/campground");
 var Comment = require("./models/comment");
 var User = require("./models/user");
 var seedDB = require("./seeds");
+var passport = require("passport");
+var LocalStratergy = require("passport-local");
+
 
 
 mongoose.connect("mongodb://localhost/yelp_camp");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
-
 seedDB();
 
-//Schema setup
+app.use(require("express-session")({
+	secret:"Kuch bhi rakh le re bhai",
+	resave: false,
+	saveUninitialized: false
+}));
 
-
-
-// Campground.create({
-//         name: "Manali",
-//         image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQO3ZjRQZaLS1QkYKgfF2ZLfN6A_JrrUHBdvQq748V-RjtfjAcm",
-//         description: "An amazing place to be. No bathrooms, no waterfall. Beautiful Manali"
-//     },
-//     function(err, campground){
-//     if(err)
-//     console.log(err);
-//     else
-//     {
-//         console.log("Newly created campground");
-//         console.log(campground);
-//     }
-// });
-
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStratergy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+app.use(function(req,res,next){
+	res.locals.currentUser = req.user;
+	next();
+});	
 
 app.get("/", function(req, res) {
   res.render("landing");
 });
 
+// INDEX - show all campgrounds
 app.get("/campgrounds", function(req, res) {
   Campground.find({}, function(err, allcampgrounds) {
     if (err) console.log(err);
     else {
-      res.render("campgrounds/Index", { campgrounds: allcampgrounds });
+      res.render("campgrounds/Index", { campgrounds: allcampgrounds, currentUser: req.user });
     }
   });
 });
@@ -84,7 +83,7 @@ app.get("/campgrounds/:id", function(req,res){
 //    COMMENTS ROUTES
 // =========================================
 
-app.get("/campgrounds/:id/comments/new", function(req,res){
+app.get("/campgrounds/:id/comments/new", isLoggedIn, function(req,res){
   Campground.findById(req.params.id,function(err,campground){
       if(err)
         console.log(err);
@@ -95,7 +94,7 @@ app.get("/campgrounds/:id/comments/new", function(req,res){
   })  
 });
 
-app.post("/campgrounds/:id/comments",function(req,res){
+app.post("/campgrounds/:id/comments", isLoggedIn, function(req,res){
 	//Lookup campground using ID
 	Campground.findById(req.params.id, function(err,campground){
 		if(err){
@@ -122,6 +121,54 @@ app.post("/campgrounds/:id/comments",function(req,res){
 	//redirect to SHOW page of campground
 });
 
+// ======================
+// AUTH ROUTES
+// ======================
+
+// Show Register form
+app.get("/register",function(req,res){
+	res.render("register");
+});
+
+// handle sign up logic
+app.post("/register", function(req, res){
+    var newUser = new User({username: req.body.username});
+    User.register(newUser, req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            return res.render("register");
+        }
+        passport.authenticate("local")(req, res, function(){
+           res.redirect("/campgrounds"); 
+        });
+    });
+});
+
+// Show login form
+app.get("/login", function(req,res){
+	res.render("login");
+});
+
+// Handling Login Logic
+app.post("/login", passport.authenticate("local",
+	{
+		successRedirect: "/campgrounds",
+		failureRedirect: "/login"
+	}),function(req,res){
+});
+
+// Logout route
+app.get("/logout",function(req,res){
+	req.logOut();
+	res.redirect("/campgrounds")
+});
+
+function isLoggedIn(req,res,next){
+	if(req.isAuthenticated()){
+		return next();
+	}
+	res.redirect("/login");
+}
 app.listen(3000, function() {
   console.log("Yelp camp has started");
 });
